e372a9ff511a2dcd323c34598b3eb3b6eb2c45f4
lliurex <juanma1980@gmail.com>
2021-12-09 17:14:36 +0100

    WIP

--- stacks/hotkeys.py
+++ stacks/hotkeys.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+from . import functionHelper
 import sys
 import os
-from PySide2.QtWidgets import QApplication, QLabel, QWidget, QPushButton,QVBoxLayout,QLineEdit,QHBoxLayout,QComboBox,QCheckBox,QTabBar,QTabWidget,QTabBar,QTabWidget
+from PySide2.QtWidgets import QApplication, QLabel, QWidget, QPushButton,QVBoxLayout,QLineEdit,QHBoxLayout,QComboBox,QCheckBox,QTabBar,QTabWidget,QTabBar,QTabWidget,QTableWidget
 from PySide2 import QtGui
-from PySide2.QtCore import Qt,QSignalMapper
+from PySide2.QtCore import Qt,Signal,QSignalMapper,QProcess,QEvent,QSize
 from appconfig.appConfigStack import appConfigStack as confStack
 import gettext
 _ = gettext.gettext
 import json
 import subprocess
-import dbus,dbus.service,dbus.exceptions
 QString=type("")
 
 i18n={
@@ -27,6 +27,7 @@ i18n={
 	}
 
 class hotkeys(confStack):
+	keybind_signal=Signal("PyObject")
 	def __init_stack__(self):
 		self.dbg=True
 		self._debug("hotkeys load")
@@ -36,122 +37,142 @@ class hotkeys(confStack):
 		self.tooltip=i18n.get('TOOLTIP')
 		self.index=1
 		self.enabled=True
-		self.defaultRepos={}
 		self.changed=[]
 		self.level='user'
-		self.bus=None
-		self.config={}
-		self.kaccessSections={"SingleClick":"KDE","ScrollbarLeftClickNavigatesByPage":"KDE"}
-		self.fileSections={"KDE":"kdeglobals"}
-		#self.kwinMethods=self._getKwinMethods()
-		self.kwinMethods={}
+		self.sysConfig={}
+		self.wrkFiles=["kglobalshortcutsrc"]
 		self.optionChanged=[]
+		self.keymap={}
+		for key,value in vars(Qt).items():
+			if isinstance(value, Qt.Key):
+				self.keymap[value]=key.partition('_')[2]
+		self.modmap={
+					Qt.ControlModifier: self.keymap[Qt.Key_Control],
+					Qt.AltModifier: self.keymap[Qt.Key_Alt],
+					Qt.ShiftModifier: self.keymap[Qt.Key_Shift],
+					Qt.MetaModifier: self.keymap[Qt.Key_Meta],
+					Qt.GroupSwitchModifier: self.keymap[Qt.Key_AltGr],
+					Qt.KeypadModifier: self.keymap[Qt.Key_NumLock]
+					}
 	#def __init__
 
 	def _load_screen(self):
+		self.installEventFilter(self)
 		self.box=QVBoxLayout()
 		self.tabBar=QTabWidget()
 		self.box.addWidget(self.tabBar,0)
 		self.setLayout(self.box)
 		sigmap_run=QSignalMapper(self)
-		sigmap_run.mapped[QString].connect(self._updateConfig)
+		sigmap_run.mapped[QString].connect(self._grab_alt_keys)
 		self.widgets={}
-		self.level='user'
+		self.widgetsText={}
 		self.refresh=True
-		self.config=self.getConfig(level=self.level)
-		config=self.config.get(self.level,{})
-		for key,item in config.get('hotkeys',{}).items():
-			if key.upper() in i18n.keys():
-				desc=i18n.get(key.upper())
-			else:
-				desc=key
-			widget=None
-			if (isinstance(item,str)):
-				if item in ["true","false"]:
-					widget=QCheckBox(desc)
-					sigmap_run.setMapping(widget,key)
-					widget.stateChanged.connect(sigmap_run.map)
-				elif key=='nextWindow':
-					self.box.addWidget(QLabel(i18n.get('NEXTWINDOW')))
-					hotkey=self._getHotkey("Walk Through Windows")
-					widget=QPushButton(hotkey)
-					sigmap_run.setMapping(widget,key)
-					widget.pressed.connect(sigmap_run.map)
-				elif key=='prevWindow':
-					self.box.addWidget(QLabel(i18n.get('PREVWINDOW')))
-					hotkey=self._getHotkey("Walk Through Windows (Reverse)")
-					widget=QPushButton(hotkey)
-					sigmap_run.setMapping(widget,key)
-					widget.pressed.connect(sigmap_run.map)
-				elif key=='closeWindow':
-					self.box.addWidget(QLabel(i18n.get('CLOSEWINDOW')))
-					hotkey=self._getHotkey("Window Close")
-					widget=QPushButton(hotkey)
-					sigmap_run.setMapping(widget,key)
-					widget.pressed.connect(sigmap_run.map)
-				elif key=='showDesktop':
-					self.box.addWidget(QLabel(i18n.get('SHOWDESKTOP')))
-					hotkey=self._getHotkey("Show Desktop")
-					widget=QPushButton(hotkey)
-					sigmap_run.setMapping(widget,key)
-					widget.pressed.connect(sigmap_run.map)
-				elif key=='launchCommand':
-					self.box.addWidget(QLabel(i18n.get('LAUNCHCOMMAND')))
-					widget=QPushButton()
-					sigmap_run.setMapping(widget,key)
-					widget.pressed.connect(sigmap_run.map)
+		tbl_hotkeys=QTableWidget(0,2)
+		tbl_hotkeys.horizontalHeader().hide()
+		tbl_hotkeys.verticalHeader().hide()
+		for wrkFile in self.wrkFiles:
+			systemConfig=functionHelper.getSystemConfig(wrkFile)
+			self.sysConfig.update(systemConfig)
+			print("***")
+			print(self.sysConfig)
+			print("***")
+			for kfile,sections in systemConfig.items():
+				for section,settings in sections.items():
+					for setting in settings:
+						(name,data)=setting
+						data=data.split(",")
+						tbl_hotkeys.insertRow(tbl_hotkeys.rowCount())
+						desc=""
+						if len(data)>0:
+							desc=data[-1]
+						lbl=QLabel(desc)
+						tbl_hotkeys.setCellWidget(tbl_hotkeys.rowCount()-1,0,lbl)
+						btn=QPushButton(data[0])
+						sigmap_run.setMapping(btn,name)
+						btn.clicked.connect(sigmap_run.map)
+						tbl_hotkeys.setCellWidget(tbl_hotkeys.rowCount()-1,1,btn)
+						self.widgets.update({name:btn})
+						self.widgetsText.update({btn:name})
 
-				else:
-					widget=QLineEdit()
-					sigmap_run.setMapping(widget,key)
-					widget.editingFinished.connect(sigmap_run.map)
-			elif (isinstance(item,list)):
-				print("{} -> List".format(item))
-			elif (isinstance(item,dict)):
-				print("{} -> Dict".format(item))
-			if widget:
-				self.widgets[key]=widget
-				self.box.addWidget(widget)
+		tbl_hotkeys.resizeColumnsToContents()
+		tbl_hotkeys.resizeRowsToContents()
+		self.box.addWidget(tbl_hotkeys)
+		lbl_add=QLabel(_("Add new"))
+		self.box.addWidget(lbl_add)
+		self.show()
 		self.updateScreen()
 	#def _load_screen
 
+	def _grab_alt_keys(self,*args):
+		desc=''
+		btn=''
+		self.btn=btn
+		if len(args)>0:
+			desc=args[0]
+		if desc:
+			btn=self.widgets.get(desc,'')
+		if btn:
+			btn.setText("")
+			self.grabKeyboard()
+			self.keybind_signal.connect(self._set_config_key)
+			self.btn=btn
+	#def _grab_alt_keys
+
+	def _set_config_key(self,keypress):
+		self.btn.setText(keypress)
+		desc=self.widgetsText.get(self.btn)
+		sysConfig=self.sysConfig.copy()
+		for kfile in self.wrkFiles:
+			for section,data in sysConfig.get(kfile,{}).items():
+				dataTmp=[]
+				for setting,value in data:
+					if setting==desc:
+						valueArray=value.split(",")
+						valueArray[0]=keypress
+						valueArray[1]=keypress
+						value=",".join(valueArray)
+					dataTmp.append((setting,value))
+				self.sysConfig[kfile][section]=dataTmp
+		#if keypress!=self.keytext:
+		#	self.changes=True
+		#	self.setChanged(self.btn_conf)
+	#def _set_config_key
+
+	def eventFilter(self,source,event):
+		sw_mod=False
+		keypressed=[]
+		if (event.type()==QEvent.KeyPress):
+			for modifier,text in self.modmap.items():
+				if event.modifiers() & modifier:
+					sw_mod=True
+					keypressed.append(text)
+			key=self.keymap.get(event.key(),event.text())
+			if key not in keypressed:
+				if sw_mod==True:
+					sw_mod=False
+				keypressed.append(key)
+			if sw_mod==False:
+				self.keybind_signal.emit("+".join(keypressed))
+		if (event.type()==QEvent.KeyRelease):
+			self.releaseKeyboard()
+
+		return False
+	#def eventFilter
+
 	def updateScreen(self):
-		self.level='user'
-		self.refresh=True
-		self.config=self.getConfig(level=self.level)
-		for section,option in self.config.get(self.level,{}).items():
-			if (isinstance(option,dict)):
-				for optionName,value in option.items():
-####				if optionName in self.kwinMethods:
-####					print("Kwin method {}".format(optionName))
-####				else:
-####					print("Item not found {}".format(optionName))
-					widget=self._getWidgetFromKey(optionName)
-					if isinstance(widget,QCheckBox):
-						state=False
-						if value.lower()=="true":
-							state=True
-						widget.setChecked(state)
-					if isinstance(widget,QLineEdit):
-						widget.setText(value)
+		pass
 	#def _udpate_screen
 
-	def _updateConfig(self,key):
-		widget=self._getWidgetFromKey(key)
-		self.setChanged(True)
-		section=self._getSectionFromKey(key)
-		if section:
-			if isinstance(widget,QCheckBox):
-				value=str(widget.isChecked()).lower()
-			elif isinstance(widget,QLineEdit):
-				value=widget.text()
-			self.config[self.level][section].update({key:value})
-		self.optionChanged.append(key)
-		self._debug("Changed: {}".format(key))
-		#	if key in self.kwinMethods:
-		#		self._exeKwinMethod(key) 
+	def _updateConfig(self,name):
+		print(name)
 
 	def writeConfig(self):
+		print(self.sysConfig)
+		for kfile,sections in self.sysConfig.items():
+			for section,data in sections.items():
+				(desc,value)=data
+				functionHelper.setKdeConfigSetting(self,section,desc,value,kfile):
+		return
 		for section,option in self.config.get(self.level,{}).items():
 			if isinstance(option,dict):
 				for name,value in option.items():
@@ -159,92 +180,3 @@ class hotkeys(confStack):
 						print(name)
 						self._exeKwinMethod(name) 
 		self.optionChanged=[]
-
-	def _getSectionFromKey(self,key):
-		sec=''
-		for section,option in self.config.get(self.level,{}).items():
-			if isinstance(option,dict):
-				if key in option.keys():
-					sec=section
-					break
-		return(sec)
-
-	def _getWidgetFromKey(self,key):
-		return(self.widgets.get(key,''))
-
-	
-	def _connect(self):
-		bus=None
-		try:
-			bus=dbus.SessionBus()
-		except Exception as e:
-			bus=None
-			print("Could not get session bus: %s\nAborting"%e)
-		return(bus)
-
-	def _getKwinMethods(self):
-		relevantMethods=[]
-		bus=dbus.SessionBus()
-		#return(getDbusObject(,,""))
-		kbus=bus.get_object("org.kde.kglobalaccel","/component/kwin")
-		interface=dbus.Interface(kbus,"org.kde.kglobalaccel.Component")
-		#method=interface.get_dbus_method("org.kde.kglobalaccel.Component.allShortcutInfos")
-		method=interface.get_dbus_method("allShortcutInfos")
-		result=method()
-		for dbusRes in result:
-			relevantMethods.append(dbusRes[0])
-		return(relevantMethods)
-
-	def _exeKwinMethod(self,method):
-		bus=dbus.SessionBus()
-		kbus=bus.get_object("org.kde.kglobalaccel","/component/kwin")
-		interface=dbus.Interface(kbus,"org.kde.kglobalaccel.Component")
-		methodCall=interface.get_dbus_method("invokeShortcut")
-		self._debug("Calling {}".format(method))
-		methodCall(method)
-
-	def _disableKwinMethod(self,method):
-		pass
-
-	def _getKdeConfigSetting(self,group,key,kfile="kaccessrc"):
-		#kfile=kaccessrc
-		kfile=self.fileSections.get(group,'kaccesrc')
-		self._debug("Reading value {} from {}".format(key,kfile))
-		cmd=["kreadconfig5","--file",os.path.join(os.environ['HOME'],".config",kfile),"--group",group,"--key",key]
-		ret='false'
-		try:
-			ret=subprocess.check_output(cmd,universal_newlines=True).strip()
-		except Exception as e:
-			print(e)
-		self._debug("Read value: {}".format(ret))
-		return(ret)
-
-	def _setKdeConfigSetting(self,group,key,value,kfile="kaccessrc"):
-		#kfile=kaccessrc
-		kfile=self.fileSections.get(group,'kaccesrc')
-		self._debug("Writing value {} from {} -> {}".format(key,kfile,value))
-		cmd=["kwriteconfig5","--file",os.path.join(os.environ['HOME'],".config",kfile),"--group",group,"--key",key,"{}".format(value)]
-		ret='false'
-		try:
-			ret=subprocess.check_output(cmd,universal_newlines=True).strip()
-		except Exception as e:
-			print(e)
-		self._debug("Write value: {}".format(ret))
-		return(ret)
-
-	def _getHotkey(self,key):
-		group="kwin"
-		if key=='invertEnabled':
-			key="Invert"
-		kfile="kglobalshortcutsrc"
-		cmd=["kreadconfig5","--file",os.path.join(os.environ['HOME'],".config",kfile),"--group",group,"--key",key]
-		ret='false'
-		try:
-			ret=subprocess.check_output(cmd,universal_newlines=True).strip()
-		except Exception as e:
-			print(e)
-		if "," in ret:
-			val=ret.split(',')
-			ret=val[0]
-		self._debug("Hotkey value: {}".format(ret))
-		return(ret)

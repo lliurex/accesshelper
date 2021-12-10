6c5535671769340bf0938a42ebcfcbad078e01ca
lliurex <juanma1980@gmail.com>
2021-12-10 16:07:30 +0100

    implement hotkey

--- stacks/access.py
+++ stacks/access.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
 import os
-from PySide2.QtWidgets import QApplication, QLabel, QWidget, QPushButton,QVBoxLayout,QLineEdit,QHBoxLayout,QComboBox,QCheckBox,QListWidget,QSizePolicy
+from PySide2.QtWidgets import QApplication, QLabel, QWidget, QPushButton,QGridLayout,QLineEdit,QHBoxLayout,QComboBox,QCheckBox,QListWidget,QSizePolicy
 from PySide2 import QtGui
-from PySide2.QtCore import Qt,QSignalMapper
+from PySide2.QtCore import Qt,Signal,QSignalMapper,QEvent
 from appconfig.appConfigStack import appConfigStack as confStack
 import gettext
 _ = gettext.gettext
@@ -29,25 +29,19 @@ i18n={
 	"INVERTENABLED":_("Invert screen colours"),
 	"INVERTWINDOW":_("Invert windows colours"),
 	"ANIMATEONCLICK":_("Show animation on click"),
-	"FOCUSFOLLOWSMOUSE":_("Follow cursor"),
-	"CLICKTOFOCUS":_("Click to focus"),
-	"SIZE":_("Font size"),
-	"FAMILY":_("Font family"),
-	"CURSORTHEME":_("Cursor theme"),
-	"CURSORSIZE":_("Cursor size"),
-	"FOLLOWPOINTER":_("Always follow the pointer"),
-	"SINGLECLICK":_("Only one click for open elements"),
-	"SCROLLBARMODE":_("Scrollbar sliding mode"),
-	"SHOWDESKTOPGRID":_("Show a grid when moving windows"),
+	"SNAPHELPERENABLED":_("Show a grid when moving windows"),
 	"LOOKINGGLASSENABLED":_("Activate eyefish effect"),
-	"MAGNIFIERENABLED":_("Activate glass effect"),
-	"ZOOMDESKTOP":_("Zoom desktop"),
-	"HOTCORNERS":_("Actions on screen corners"),
+	"MAGNIFIERENABLED":_("Glass effect"),
+	"ZOOMENABLED":_("Zoom effect"),
 	"SYSTEMBELL":_("Acoustic system bell"),
-	"FOCUSPOLICY":_("Set the policy focus")
+	"FOCUSPOLICY":_("Set the policy focus"),
+	"VISIBLEBELL":_("Visible bell"),
+	"TRACKMOUSEENABLED":_("Track pointer"),
+	"MOUSECLICKENABLED":_("Track click")
 	}
 
 class access(confStack):
+	keybind_signal=Signal("PyObject")
 	def __init_stack__(self):
 		self.dbg=True
 		self._debug("access Load")
@@ -57,64 +51,138 @@ class access(confStack):
 		self.tooltip=i18n.get('TOOLTIP')
 		self.index=1
 		self.enabled=True
-		self.defaultRepos={}
 		self.changed=[]
 		self.level='user'
-		self.bus=None
 		self.config={}
 		self.sysConfig={}
-		#self.kwinMethods=self._getKwinMethods()
-		self.wrkFiles=["kwinrc"]
-		self.blockSettings={"kwinrc":["FocusPolicy"]}
+		self.wrkFiles=["kaccesrc","kwinrc"]
+		self.blockSettings={"kwinrc":["FocusPolicy","lookingglassEnabled"]}
 		self.wantSettings={}
 		self.widgets={}
-		self.kwinMethods={}
-		self.kaccessSections={"SystemBell":"Bell","invertEnabled":"Plugins","SingleClick":"KDE"}
-		self.fileSections={"Bell":"kaccessrc","Plugins":"kwinrc","KDE":"kdeglobals"}
+		self.widgetsText={}
 		self.optionChanged=[]
+		self.keymap={}
+		for key,value in vars(Qt).items():
+			if isinstance(value, Qt.Key):
+				self.keymap[value]=key.partition('_')[2]
+		self.modmap={
+					Qt.ControlModifier: self.keymap[Qt.Key_Control],
+					Qt.AltModifier: self.keymap[Qt.Key_Alt],
+					Qt.ShiftModifier: self.keymap[Qt.Key_Shift],
+					Qt.MetaModifier: self.keymap[Qt.Key_Meta],
+					Qt.GroupSwitchModifier: self.keymap[Qt.Key_AltGr],
+					Qt.KeypadModifier: self.keymap[Qt.Key_NumLock]
+					}
 	#def __init__
 
-	def cssStyle(self):
-		style="""
-			QLabel{
-				margin:96px;
-				margin-right:6px;
-			}
-			QCheckBox{
-				margin:13px;
-			}
-		"""
-		return(style)
-
 	def _load_screen(self):
-		self.box=QVBoxLayout()
+		self.installEventFilter(self)
+		self.box=QGridLayout()
 		self.setLayout(self.box)
+		sigmap_run=QSignalMapper(self)
+		sigmap_run.mapped[QString].connect(self._grab_alt_keys)
 		self.refresh=True
+		row,col=(0,0)
 		for wrkFile in self.wrkFiles:
 			systemConfig=functionHelper.getSystemConfig(wrkFile)
 			self.sysConfig.update(systemConfig)
 			for kfile,sections in systemConfig.items():
+				want=self.wantSettings.get(kfile,[])
+				block=self.blockSettings.get(kfile,[])
 				for section,settings in sections.items():
 					for setting in settings:
 						(name,data)=setting
-						want=self.wantSettings.get(kfile,[])
-						block=self.blockSettings.get(kfile,[])
 						if name in block or (len(want)>0 and name not in want):
 							continue
 						desc=i18n.get(name.upper(),name)
 						lbl=QLabel(desc)
 						if (data.lower() in ("true","false")) or (data==''):
 							btn=QCheckBox(desc)
+							#btn=QPushButton(desc)
+							#btn.setAutoDefault(False)
+							#btn.setDefault(False)
+							#btn.setCheckable(True)
+							#btn.setStyleSheet(functionHelper.cssStyle())
 							state=False
 							if data.lower()=="true":
 								state=True
 							btn.setChecked(state)
-							self.box.addWidget(btn)
 							self.widgets.update({name:btn})
+							self.box.addWidget(btn,row,col)
+							col+=1
+							btnName=functionHelper.getHotkey(name)
+							if btnName:
+								btn=QPushButton(btnName)
+								sigmap_run.setMapping(btn,btnName)
+								self.widgets.update({btnName:btn})
+								self.widgetsText.update({btn:btnName})
+								btn.clicked.connect(sigmap_run.map)
+								self.box.addWidget(btn,row,col,Qt.Alignment(1))
+							col+=1
+							if col==2:
+								row+=1
+								col=0
+
 						#ibtn.clicked
 		#lst_settings.resizeRowsToContents()
 		self.updateScreen()
 	#def _load_screen
+
+	def _grab_alt_keys(self,*args):
+		desc=''
+		btn=''
+		self.btn=btn
+		if len(args)>0:
+			desc=args[0]
+		if desc:
+			btn=self.widgets.get(desc,'')
+		if btn:
+			btn.setText("")
+			self.grabKeyboard()
+			self.keybind_signal.connect(self._set_config_key)
+			self.btn=btn
+	#def _grab_alt_keys
+
+	def _set_config_key(self,keypress):
+		keypress=keypress.replace("Control","Ctrl")
+		self.btn.setText(keypress)
+		desc=self.widgetsText.get(self.btn)
+		sysConfig=self.sysConfig.copy()
+		for kfile in self.wrkFiles:
+			for section,data in sysConfig.get(kfile,{}).items():
+				dataTmp=[]
+				for setting,value in data:
+					if setting==desc:
+						valueArray=value.split(",")
+						valueArray[0]=keypress
+						valueArray[1]=keypress
+						value=",".join(valueArray)
+					dataTmp.append((setting,value))
+				self.sysConfig[kfile][section]=dataTmp
+		#if keypress!=self.keytext:
+		#	self.changes=True
+		#	self.setChanged(self.btn_conf)
+	#def _set_config_key
+	def eventFilter(self,source,event):
+		sw_mod=False
+		keypressed=[]
+		if (event.type()==QEvent.KeyPress):
+			for modifier,text in self.modmap.items():
+				if event.modifiers() & modifier:
+					sw_mod=True
+					keypressed.append(text)
+			key=self.keymap.get(event.key(),event.text())
+			if key not in keypressed:
+				if sw_mod==True:
+					sw_mod=False
+				keypressed.append(key)
+			if sw_mod==False:
+				self.keybind_signal.emit("+".join(keypressed))
+		if (event.type()==QEvent.KeyRelease):
+			self.releaseKeyboard()
+
+		return False
+	#def eventFilter
 
 	def updateScreen(self):
 		return
@@ -139,15 +207,6 @@ class access(confStack):
 				self.sysConfig[kfile][section]=dataTmp
 
 		functionHelper.setSystemConfig(self.sysConfig)
-		return
-		for section,option in self.config.get(self.level,{}).items():
-			if isinstance(option,dict):
-				for name,value in option.items():
-					if name in self.optionChanged:
-						if name in self.kaccessSections:
-							self._debug("Setting {} -> {}".format(name,value))
-							self._setKdeConfigSetting(group=self.kaccessSections.get(name),key=name,value=value)
-						#self._exeKwinMethod(name) 
 		self._reloadConfig()
 		self.optionChanged=[]
-
+		return

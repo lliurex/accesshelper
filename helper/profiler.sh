0a2e6a2dbab1c0555ce72b42e6c3ec63dcdc8306
lliurex <juanma1980@gmail.com>
2021-12-11 21:14:45 +0100

    fix trailing line

--- helper/profiler.sh
+++ helper/profiler.sh
@@ -1,10 +1,8 @@
- #!/bin/bash
- #Copyright 2021 Team LliureX
- #Simple script to copy profiles to app dir
- WRKDIR="/usr/share/acceshelper/profiles"
- DESTPATH=${WRKDIR}/$(basename $2)
- 
- mkdir $DESTPATH 2>/dev/null
- cp -r $1 $DESTPATH
+#!/bin/bash
+#Copyright 2021 Team LliureX
+#Simple script to copy profiles to app dir
+WRKDIR="/usr/share/acceshelper/profiles"
+DESTPATH=${WRKDIR}/$(basename $2)
 
- #And that's all
+cp -r $1 $DESTPATH
+#And that's all

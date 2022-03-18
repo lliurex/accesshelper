cb345b848f121fba2674271df6b0b82a20e44732
lliurex <juanma1980@gmail.com>
2022-03-18 12:38:19 +0100

    WIP on set font size

--- src/accessdock.py
+++ src/accessdock.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import os,sys
-from PySide2.QtWidgets import QApplication,QMessageBox,QGridLayout,QLabel,QPushButton,QWidget,QFrame
+from PySide2.QtWidgets import QApplication,QMessageBox,QGridLayout,QLabel,QPushButton,QWidget,QFrame,QDialog
 from PySide2.QtCore import Qt,QSignalMapper
 from stacks import libaccesshelper
+from appconfig.appConfigStack import appConfigStack as confStack
+from stacks.alpha import alpha
 import gettext
 import signal
 import time
@@ -12,13 +14,9 @@ _ = gettext.gettext
 QString=type("")
 QInt=type(0)
 
-
-
 class accessdock(QWidget):
-
 	def __init__(self,*args,**kwargs):
 		super().__init__()
-		signal.signal(signal.SIGUSR1,self.handler)
 		self.dbg=True
 		self.menu=App2Menu.app2menu()
 		self.confFile="accessdock.json"
@@ -36,8 +34,6 @@ class accessdock(QWidget):
 		if self.dbg:
 			print("dock: {}".format(msg))
 	#def _debug
-	def handler(self,signal_received, frame):
-		print('SIGINT or CTRL-C detected. Exiting gracefully')
 
 	def _loadConfig(self):
 		config=self._readConfig()
@@ -69,6 +65,7 @@ class accessdock(QWidget):
 		data.append(("_k_friendly_name","accessdock"))
 		config={'kglobalshortcutsrc':{'accessdock.desktop':data}}
 		self.accesshelper.setSystemConfig(config)
+	#def _setKdeHotkey
 
 	def _renderGui(self):
 		#self.setWindowFlags(Qt.X11BypassWindowManagerHint)
@@ -86,14 +83,14 @@ class accessdock(QWidget):
 			lbl=QLabel(setting.replace("_"," ").capitalize())
 			layout2.addWidget(lbl,0,col,1,1)
 			btn=QPushButton()
+			if setting=="font_size":
+				btn.setText("{:.0f}".format(self.font().pointSizeF()))
 			sigmap_run.setMapping(btn,setting)
 			btn.clicked.connect(sigmap_run.map)
 			layout2.addWidget(btn,1,col,1,1)
 			self.widgets[setting]=btn
 			col+=1
-
 		self.setLayout(layout)
-		print("MOVE TO {} {}".format(self.coordx,self.coordy))
 		self.move(self.coordx,self.coordy)
 	#def _renderGui
 
@@ -101,7 +98,75 @@ class accessdock(QWidget):
 		if isinstance(args,tuple):
 			if args[0].lower()=="hide":
 				self.close()
+			elif args[0].lower()=="color":
+				alphaDlg=alpha(alpha)
+				alphaDlg.move(self.coordx,self.coordy)
+				alphaDlg._load_screen()
+				alphaDlg.btn_ok.clicked.connect(alphaDlg.close)
+				alphaDlg.btn_cancel.clicked.connect(alphaDlg.close)
+				alphaDlg.setWindowModality(Qt.WindowModal)
+				alphaDlg.setWindowFlags(Qt.NoDropShadowWindowHint|Qt.WindowStaysOnTopHint|Qt.FramelessWindowHint)
+				alphaDlg.show()
+			elif args[0].lower()=="font_size":
+				self._fontSize()
 	#def execute
+
+	def _fontSize(self):
+		def moreSize(*args):
+			font=lblTest.font()
+			size=font.pointSizeF()+1
+			font.setPointSizeF(size)
+			lblTest.setFont(font)
+		def lessSize(*args):
+			font=lblTest.font()
+			size=font.pointSizeF()-1
+			font.setPointSizeF(size)
+			lblTest.setFont(font)
+			
+		dlg=QDialog()
+		lay=QGridLayout()
+		dlg.setLayout(lay)
+		frame=QFrame()
+		frame.setFrameShape(QFrame.Panel)
+		lay.addWidget(frame)
+		lay2 = QGridLayout(frame)
+		btnPlus=QPushButton("+")
+		btnPlus.clicked.connect(moreSize)
+		lay2.addWidget(btnPlus,0,0,1,1)
+		btnMinus=QPushButton("-")
+		btnMinus.clicked.connect(lessSize)
+		lay2.addWidget(btnMinus,1,0,1,1)
+		lblTest=QLabel("Texto de prueba")
+		lblTest.setWordWrap(True)
+		lay2.addWidget(lblTest,0,2,2,1)
+		btnCancel=QPushButton("Cancel")
+		btnCancel.clicked.connect(dlg.close)
+		lay2.addWidget(btnCancel,2,0,2,1)
+		btnOk=QPushButton("Apply")
+		btnOk.clicked.connect(dlg.accept)
+		lay2.addWidget(btnOk,2,2,1,1)
+		dlg.move(self.coordx,self.coordy)
+		dlg.setWindowModality(Qt.WindowModal)
+		dlg.setWindowFlags(Qt.NoDropShadowWindowHint|Qt.WindowStaysOnTopHint|Qt.FramelessWindowHint)
+		change=dlg.exec()
+		if change:
+			qfont=lblTest.font()
+			font=qfont.toString()
+			minfont=font
+			size=qfont.pointSize()
+			minSize=size-2
+			fontFixed="Hack"
+			fixed="{0},{1},-1,5,50,0,0,0,0,0".format(fontFixed,size)
+			if size>8:
+				qfont.setPointSize(size-2)
+				minFont=qfont.toString()
+			self.accesshelper.setKdeConfigSetting("General","fixed",fixed,"kdeglobals")
+			self.accesshelper.setKdeConfigSetting("General","font",font,"kdeglobals")
+			self.accesshelper.setKdeConfigSetting("General","menuFont",font,"kdeglobals")
+			self.accesshelper.setKdeConfigSetting("General","smallestReadableFont",minFont,"kdeglobals")
+			self.accesshelper.setKdeConfigSetting("General","toolBarFont",font,"kdeglobals")
+			self.accesshelper.setKdeConfigSetting("Appearance","Font",fixed,"Lliurex.profile")
+			self.widgets["font_size"].setText("{:.0f}".format(size))
 
 	def mousePressEvent(self, event):
 		event.accept()
@@ -121,9 +186,10 @@ class accessdock(QWidget):
 			with open(os.path.join(os.environ.get('HOME'),".config",self.confFile),'r') as f:
 				config.update(json.loads(f.read()))
 		config={"coords":[x,y]}
+		self.coordx,self.coordy=(x,y)
 		with open(os.path.join(os.environ.get('HOME'),".config",self.confFile),'w') as f:
 			f.write(json.dumps(config,indent=4))
-	#def mousePressEvent
+	#def mouseReleaseEvent
 
 if os.path.isfile("/tmp/.accessdock.pid"):
 	kill=True

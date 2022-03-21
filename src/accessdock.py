db241c448e903017765717f04e4a27ce6e7fd3fd
lliurex <juanma1980@gmail.com>
2022-03-21 13:05:38 +0100

    WIP on beta

--- src/accessdock.py
+++ src/accessdock.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import os,sys
-from PySide2.QtWidgets import QApplication,QMessageBox,QGridLayout,QLabel,QPushButton,QWidget,QFrame,QDialog
-from PySide2.QtCore import Qt,QSignalMapper
+from PySide2.QtWidgets import QApplication,QMessageBox,QGridLayout,QLabel,QToolButton,QWidget,QFrame,QDialog,QPushButton
+from PySide2.QtCore import Qt,QSignalMapper,QByteArray,QSize
+from PySide2.QtGui import QIcon,QPixmap,QCursor
+import dbus
+import dbus.mainloop.glib
 from stacks import libaccesshelper
 from appconfig.appConfigStack import appConfigStack as confStack
 from stacks.alpha import alpha
 import gettext
-import signal
 import time
 import json
+import subprocess
 from app2menu import App2Menu
 _ = gettext.gettext
 QString=type("")
@@ -80,14 +83,36 @@ class accessdock(QWidget):
 		sigmap_run=QSignalMapper(self)
 		sigmap_run.mapped[QString].connect(self.execute)
 		for setting,value in self.fastSettings.items():
-			lbl=QLabel(setting.replace("_"," ").capitalize())
-			layout2.addWidget(lbl,0,col,1,1)
-			btn=QPushButton()
+			#lbl=QLabel(setting.replace("_"," ").capitalize())
+			#layout2.addWidget(lbl,0,col,1,1)
+			btn=QToolButton()
+			btn.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
 			if setting=="font_size":
-				btn.setText("{:.0f}".format(self.font().pointSizeF()))
+				btn.setText("{:.0f}\nFont".format(self.font().pointSizeF()))
+			elif setting=="hide":
+				icn=QIcon.fromTheme("view-hidden")
+				btn.setText(_("Hide"))
+				btn.setIcon(icn)
+			elif setting=="config":
+				icn=QIcon.fromTheme("preferences")
+				btn.setText(_("Configure"))
+				btn.setIcon(icn)
+			elif setting=="read":
+				icn=QIcon.fromTheme("audio-ready")
+				btn.setText(_("Read screen"))
+				btn.setIcon(icn)
+			elif setting=="pointer_size":
+				icn=QIcon.fromTheme("pointer")
+				btn.setText(_("Pointer"))
+				btn.setIcon(icn)
+			elif setting=="color":
+				icn=QIcon.fromTheme("preferences-desktop-screensaver")
+				btn.setText(_("Filter"))
+				btn.setIcon(icn)
+
 			sigmap_run.setMapping(btn,setting)
 			btn.clicked.connect(sigmap_run.map)
-			layout2.addWidget(btn,1,col,1,1)
+			layout2.addWidget(btn,0,col,1,1)
 			self.widgets[setting]=btn
 			col+=1
 		self.setLayout(layout)
@@ -108,8 +133,112 @@ class accessdock(QWidget):
 				alphaDlg.setWindowFlags(Qt.NoDropShadowWindowHint|Qt.WindowStaysOnTopHint|Qt.FramelessWindowHint)
 				alphaDlg.show()
 			elif args[0].lower()=="font_size":
-				self._fontSize()
+				self._fontCursorSize("font")
+			elif args[0].lower()=="pointer_size":
+				self._fontCursorSize("pointer")
+			elif args[0].lower()=="read":
+				self._readScreen()
+			elif args[0].lower()=="config":
+				self.hide()
+				subprocess.run(["accesshelper"])
+				self.show()
 	#def execute
+	def _fontCursorSize(self,setting):
+		def moreFontSize(*args):
+			font=lblTest.font()
+			size=font.pointSizeF()+1
+			font.setPointSizeF(size)
+			lblTest.setFont(font)
+		def lessFontSize(*args):
+			font=lblTest.font()
+			size=font.pointSizeF()-1
+			font.setPointSizeF(size)
+			lblTest.setFont(font)
+		def moreCursorSize(*args):
+			pixmap=lblTest.pixmap()
+			size=pixmap.size().width()
+			if size in sizes:
+				if size<sizes[-1]:
+					size=sizes[sizes.index(size)+1]
+					pixmap=img[0].pixmap(QSize(size,size))
+					lblTest.setPixmap(pixmap)
+					cursor=QCursor(pixmap,0,0)
+					dlg.setCursor(cursor)
+		def lessCursorSize(*args):
+			pixmap=lblTest.pixmap()
+			size=pixmap.size().width()
+			if size in sizes:
+				if size>sizes[0]:
+					size=sizes[sizes.index(size)-1]
+					pixmap=img[0].pixmap(QSize(size,size))
+					lblTest.setPixmap(pixmap)
+					cursor=QCursor(pixmap,0,0)
+					dlg.setCursor(cursor)
+			
+		dlg=QDialog()
+		lay=QGridLayout()
+		dlg.setLayout(lay)
+		frame=QFrame()
+		frame.setFrameShape(QFrame.Panel)
+		lay.addWidget(frame)
+		lay2 = QGridLayout(frame)
+		btnPlus=QPushButton("+")
+		lay2.addWidget(btnPlus,0,0,1,1)
+		btnMinus=QPushButton("-")
+		lay2.addWidget(btnMinus,1,0,1,1)
+		if str(setting)=="font":
+			btnPlus.clicked.connect(moreFontSize)
+			btnMinus.clicked.connect(lessFontSize)
+			lblTest=QLabel("Texto de prueba")
+			lblTest.setWordWrap(True)
+		else:
+			btnPlus.clicked.connect(moreCursorSize)
+			btnMinus.clicked.connect(lessCursorSize)
+			img=self.accesshelper.getPointerImage()
+			qsizes=img[1]
+			sizes=[]
+			for qsize in qsizes:
+				sizes.append(qsize.width())
+			sizes.sort()
+			pixmap=img[0].pixmap(QSize(32,32))
+			lblTest=QLabel()
+			lblTest.setPixmap(pixmap)
+		lay2.addWidget(lblTest,0,1,2,1)
+		btnCancel=QPushButton("Cancel")
+		btnCancel.clicked.connect(dlg.close)
+		lay2.addWidget(btnCancel,2,0,1,1)
+		btnOk=QPushButton("Apply")
+		btnOk.clicked.connect(dlg.accept)
+		lay2.addWidget(btnOk,2,1,1,1)
+		dlg.move(self.coordx,self.coordy)
+		dlg.setWindowModality(Qt.WindowModal)
+		dlg.setWindowFlags(Qt.NoDropShadowWindowHint|Qt.WindowStaysOnTopHint|Qt.FramelessWindowHint)
+		change=dlg.exec()
+		if change:
+			if str(setting)=="fonts":
+				qfont=lblTest.font()
+				self._saveFont(qfont)
+			else:
+				self.accesshelper.setCursorSize(lblTest.pixmap().size().width())
+				self.accesshelper.setCursor()
+
+	def _saveFont(self,qfont):
+		font=qfont.toString()
+		minfont=font
+		size=qfont.pointSize()
+		minSize=size-2
+		fontFixed="Hack"
+		fixed="{0},{1},-1,5,50,0,0,0,0,0".format(fontFixed,size)
+		if size>8:
+			qfont.setPointSize(size-2)
+			minFont=qfont.toString()
+		self.accesshelper.setKdeConfigSetting("General","fixed",fixed,"kdeglobals")
+		self.accesshelper.setKdeConfigSetting("General","font",font,"kdeglobals")
+		self.accesshelper.setKdeConfigSetting("General","menuFont",font,"kdeglobals")
+		self.accesshelper.setKdeConfigSetting("General","smallestReadableFont",minFont,"kdeglobals")
+		self.accesshelper.setKdeConfigSetting("General","toolBarFont",font,"kdeglobals")
+		self.accesshelper.setKdeConfigSetting("Appearance","Font",fixed,"Lliurex.profile")
+		self.widgets["font_size"].setText("{:.0f}/nFont".format(size))
 
 	def _fontSize(self):
 		def moreSize(*args):
@@ -166,7 +295,33 @@ class accessdock(QWidget):
 			self.accesshelper.setKdeConfigSetting("General","smallestReadableFont",minFont,"kdeglobals")
 			self.accesshelper.setKdeConfigSetting("General","toolBarFont",font,"kdeglobals")
 			self.accesshelper.setKdeConfigSetting("Appearance","Font",fixed,"Lliurex.profile")
-			self.widgets["font_size"].setText("{:.0f}".format(size))
+			self.widgets["font_size"].setText("{:.0f}/nFont".format(size))
+
+	def _readScreen(self,*args):
+		dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+		self._debug("Setting dbus")
+		bus=dbus.SessionBus()
+		#register your signal callback
+		bus.add_signal_receiver(self._screenshotHandler,
+			bus_name='org.kde.KWin',
+			interface_keyword='Screenshot',
+			member_keyword='screenshotCreated',
+			path_keyword='/Screenshot',
+			message_keyword='msg')
+
+	def _screenshotHandler(self,*args,**kwargs):
+		print("args: {}".format(args))
+		print("kwargs: {}".format(kwargs))
+		bsize=args[0]
+		bsize=bsize.bit_length()
+		print(bsize)
+		data=QByteArray(args[0].to_bytes(bsize,"little"))
+		print(data)
+		a=QPixmap()
+		a.loadFromData(data)
+		print(a)
+		
+
 
 	def mousePressEvent(self, event):
 		event.accept()

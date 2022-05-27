b85fac56e3ed427d33eb33916100dae1d89f2849
juanma <juanma1980@gmail.com>
2022-05-27 13:09:58 +0200

    Wip on redo

--- src/stacks/access.py
+++ src/stacks/access.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-import sys
+from . import libaccesshelper
+from . import libhotkeys
 import os
-from PySide2.QtWidgets import QApplication, QLabel, QWidget, QPushButton,QGridLayout,QLineEdit,QVBoxLayout,QComboBox,QCheckBox,QListWidget,QSizePolicy,QRadioButton,QFrame
+from PySide2.QtWidgets import QApplication,QLabel,QGridLayout,QCheckBox,QSizePolicy,QRadioButton,QTableWidget,QHeaderView
 from PySide2 import QtGui
-from PySide2.QtCore import Qt,Signal,QSignalMapper,QEvent
+from PySide2.QtCore import Qt
 from appconfig.appConfigStack import appConfigStack as confStack
 import gettext
-import json
-import subprocess
-import dbus,dbus.service,dbus.exceptions
-from . import libaccesshelper
 _ = gettext.gettext
-QString=type("")
 
 i18n={
 	"COLOURS":_("Colours"),
@@ -63,9 +59,8 @@ actionHk={
 	"MOUSECLICKENABLED":"ToggleMouseClick"
 	}
 class access(confStack):
-	keybind_signal=Signal("PyObject")
 	def __init_stack__(self):
-		self.dbg=False
+		self.dbg=True
 		self._debug("access Load")
 		self.menu_description=i18n.get('MENUDESCRIPTION')
 		self.description=i18n.get('DESCRIPTION')
@@ -80,51 +75,77 @@ class access(confStack):
 		self.wrkFiles=["kaccesrc","kwinrc"]
 		self.blockSettings={"kwinrc":["FocusPolicy"]}
 		self.wantSettings={}
-		self.widgets={}
-		self.widgetsText={}
 		self.optionChanged=[]
-		self.keymap={}
 		self.chkbtn={}
-		for key,value in vars(Qt).items():
-			if isinstance(value, Qt.Key):
-				self.keymap[value]=key.partition('_')[2]
-		self.modmap={
-					Qt.ControlModifier: self.keymap[Qt.Key_Control],
-					Qt.AltModifier: self.keymap[Qt.Key_Alt],
-					Qt.ShiftModifier: self.keymap[Qt.Key_Shift],
-					Qt.MetaModifier: self.keymap[Qt.Key_Meta],
-					Qt.GroupSwitchModifier: self.keymap[Qt.Key_AltGr],
-					Qt.KeypadModifier: self.keymap[Qt.Key_NumLock]
-					}
 		self.accesshelper=libaccesshelper.accesshelper()
 	#def __init__
 
+	def sortArraySettings(self,settings):
+		ordArray=[]
+		ordDict={}
+		for name,value in settings:
+			if "bell" in name.lower():
+				bell=ordDict.get('bell',[])
+				bell.append((name,value))
+				ordDict['bell']=bell
+			elif "mouse" in name.lower():
+				mouse=ordDict.get('mouse',[])
+				mouse.append((name,value))
+				ordDict['mouse']=mouse
+			else:
+				other=ordDict.get('other',[])
+				other.append((name,value))
+				ordDict['other']=other
+		for key,item in ordDict.items():
+			ordArray.extend(item)
+			
+		return(ordArray)
+
 	def _load_screen(self):
-		def sortArraySettings(settings):
-			ordArray=[]
-			ordDict={}
-			for name,value in settings:
-				if "bell" in name.lower():
-					bell=ordDict.get('bell',[])
-					bell.append((name,value))
-					ordDict['bell']=bell
-				elif "mouse" in name.lower():
-					mouse=ordDict.get('mouse',[])
-					mouse.append((name,value))
-					ordDict['mouse']=mouse
-				else:
-					other=ordDict.get('other',[])
-					other.append((name,value))
-					ordDict['other']=other
-			for key,item in ordDict.items():
-				ordArray.extend(item)
-				
-			return(ordArray)
-		self.installEventFilter(self)
 		self.box=QGridLayout()
 		self.setLayout(self.box)
+		self.tblGrid=QTableWidget()
+		self.tblGrid.setColumnCount(2)
+		self.tblGrid.setShowGrid(False)
+		self.tblGrid.verticalHeader().hide()
+		self.tblGrid.verticalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+		self.tblGrid.horizontalHeader().hide()
+		self.tblGrid.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+		self.tblGrid.setSelectionBehavior(self.tblGrid.SelectRows)
+		self.tblGrid.setSelectionMode(self.tblGrid.SingleSelection)
+		self.box.addWidget(self.tblGrid)
 		self.refresh=True
-		row,col=(0,0)
+	#def _load_screen
+
+	def _testHotkey(self,hotkey):
+		if not hotkey.get("action","")=="":
+			try:
+				self.showMsg("{0} {1} {2}".format(hotkey.get("hotkey"),i18n.get("HKASSIGNED"),hotkey.get("action")))
+			except:
+				pass
+			btn=self.tblGrid.cellWidget(self.tblGrid.currentRow(),1)
+			btn.revertHotkey()
+		self.btn_ok.setEnabled(True)
+		self.btn_cancel.setEnabled(True)
+	#def _testHotkey
+
+	def _updateButtons(self):
+		for chk,btn in self.chkbtn.items():
+			if isinstance(chk,QCheckBox):
+				if chk.isChecked():
+					if isinstance(btn,libhotkeys.QHotkeyButton):
+						if btn.isVisible():
+							btn.setEnabled(True)
+				else:
+					if isinstance(btn,libhotkeys.QHotkeyButton):
+						if btn.isVisible():
+							btn.setEnabled(False)
+
+	def updateScreen(self):
+		self.tblGrid.clear()
+		self.tblGrid.setRowCount(0)
+		self.chkbtn={}
+		zoomOptions=[]
 		for wrkFile in self.wrkFiles:
 			plasmaConfig=self.accesshelper.getPlasmaConfig(wrkFile)
 			self.plasmaConfig.update(plasmaConfig)
@@ -132,9 +153,7 @@ class access(confStack):
 				want=self.wantSettings.get(kfile,[])
 				block=self.blockSettings.get(kfile,[])
 				for section,settings in sections.items():
-					zoomOptions=[]
-					settings=sortArraySettings(settings)
-
+		#			settings=self.sortArraySettings(settings)
 					for setting in settings:
 						(name,data)=setting
 						if name in block or (len(want)>0 and name not in want):
@@ -142,177 +161,44 @@ class access(confStack):
 						if name.upper() in ["MAGNIFIERENABLED","ZOOMENABLED","LOOKINGGLASSENABLED"]:
 							zoomOptions.append(setting)
 							continue
+						row=self.tblGrid.rowCount()
+						self.tblGrid.setRowCount(row+1)
 						desc=i18n.get(name.upper(),name)
 						chk=QCheckBox(desc)
 						chk.stateChanged.connect(self._updateButtons)
-						self.widgets.update({name:chk})
-						self.box.addWidget(chk,row,0)
+						self.tblGrid.setCellWidget(row,0,chk)
 						(mainHk,hkData,hkSetting,hkSection)=self.accesshelper.getHotkey(name)
 						if mainHk=="none":
 							mainHk=""
-						btn=QPushButton(mainHk)
-						name="btn_{}".format(name)
 						if mainHk=="":
 							mainHk=name
-						self.widgets.update({name:btn})
-						self.widgetsText.update({btn:{'mainHk':mainHk,'hkData':hkData,'hkSetting':hkSetting,'hkSection':hkSection}})
-						self.box.addWidget(btn,row,1)
-						btn.setEnabled(False)
-						if name.replace("btn_","").upper() not in ["SYSTEMBELL","VISIBLEBELL","SNAPHELPERENABLED"]:
+						if name.upper() not in ["SYSTEMBELL","VISIBLEBELL","SNAPHELPERENABLED"]:
+							btn=libhotkeys.QHotkeyButton(mainHk)
+							btn.hotkeyAssigned.connect(self._testHotkey)
 							self.chkbtn[chk]=btn
-						else:
-							btn.hide()
-						row+=1
-					lbl=None
-					lay=QVBoxLayout()
-					for setting in zoomOptions:
-						if lbl==None:
-							lbl=QLabel("Meta+=/Meta+-")
-							chk=QCheckBox(i18n.get("MAGNIFIEREFFECTS"))
-							chk.stateChanged.connect(self._setZoomOptionsEnabled)
-							self.widgets.update({"magnifier":chk})
-							self.box.addWidget(chk,row,0,1,1)
-							row+=1
-							self.box.addWidget(lbl,row,1,3,1)
-						(name,data)=setting
-						desc=i18n.get(name.upper(),name)
-						btn=QRadioButton(desc)
-						self.widgets.update({name:btn})
-						self.box.addWidget(btn,row,0,1,1)
-						row+=1
-		self.updateScreen()
-	#def _load_screen
-
-	def _updateButtons(self):
-		for chk,btn in self.chkbtn.items():
-			if isinstance(chk,QCheckBox):
-				if chk.isChecked():
-					if isinstance(btn,QPushButton):
-						if btn.isVisible():
-							btn.setEnabled(True)
-				else:
-					if isinstance(btn,QPushButton):
-						if btn.isVisible():
+							self.tblGrid.setCellWidget(row,1,btn)
 							btn.setEnabled(False)
-
-	def _grab_alt_keys(self,*args):
-		desc=''
-		btn=''
-		self.txt=""
-		self.btn=btn
-		if len(args)>0:
-			desc=args[0]
-		if desc:
-			btn=self.widgets.get(desc,'')
-		if btn:
-			self.text=btn.text()
-			btn.setText("Press keys")
-			self.grabKeyboard()
-			self.keybind_signal.connect(self._set_config_key)
-			self.btn=btn
-	#def _grab_alt_keys
-
-	def _set_config_key(self,keypress):
-		keypress=keypress.replace("Control","Ctrl")
-		self.btn.setText(keypress)
-		desc=self.widgetsText.get(self.btn).get("mainHk")
-		plasmaConfig=self.plasmaConfig.copy()
-		if keypress=="Ctrl" or keypress=="Alt" or keypress=="Meta":
-			return
-		action=self.accesshelper.getSettingForHotkey(keypress)
-		if action!="":
-			try:
-				self.showMsg("{0} {1} {2}".format(keypress,i18n.get("HKASSIGNED"),action))
-			except:
-				pass
-			self.btn.setText(self.text)
-		else:
-			for kfile in self.wrkFiles:
-				for section,data in plasmaConfig.get(kfile,{}).items():
-					dataTmp=[]
-					for setting,value in data:
-						if setting==desc:
-							valueArray=value.split(",")
-							valueArray[0]=keypress
-							valueArray[1]=keypress
-							value=",".join(valueArray)
-						dataTmp.append((setting,value))
-					self.plasmaConfig[kfile][section]=dataTmp
-		#if keypress!=self.keytext:
-		#	self.changes=True
-		#	self.setChanged(self.btn_conf)
-		self.btn_ok.setEnabled(True)
-		self.btn_cancel.setEnabled(True)
-	#def _set_config_key
-
-	def eventFilter(self,source,event):
-		sw_mod=False
-		keypressed=[]
-		if (event.type()==QEvent.KeyPress):
-			for modifier,text in self.modmap.items():
-				if event.modifiers() & modifier:
-					sw_mod=True
-					keypressed.append(text)
-			key=self.keymap.get(event.key(),event.text())
-			if key not in keypressed:
-				if sw_mod==True:
-					sw_mod=False
-				keypressed.append(key)
-			if sw_mod==False:
-				self.keybind_signal.emit("+".join(keypressed))
-		if (event.type()==QEvent.KeyRelease):
-			self.releaseKeyboard()
-
-		return False
-	#def eventFilter
-
-	def updateScreen(self):
-		sigmap_run=QSignalMapper(self)
-		sigmap_run.mapped[QString].connect(self._grab_alt_keys)
-		for wrkFile in self.wrkFiles:
-			plasmaConfig=self.accesshelper.getPlasmaConfig(wrkFile)
-			self.plasmaConfig.update(plasmaConfig)
-		for kfile,sections in self.plasmaConfig.items():
-			want=self.wantSettings.get(kfile,[])
-			block=self.blockSettings.get(kfile,[])
-			for section,settings in sections.items():
-				zoomOptions=[]
-				for setting in settings:
-					(name,data)=setting
-					if name in block or (len(want)>0 and name not in want):
-						continue
-					if (data.lower() in ("true","false")) or (data==''):
-						state=False
-						if data.lower()=="true":
-							state=True
-						if name:
-							wdg=self.widgets.get(name)
-							if isinstance(wdg,QRadioButton):
-								if state==True:
-									self.widgets.get("magnifier").setChecked(True)
-							else:
-								wdg.setChecked(state)
-
-					(mainHk,hkData,hkSetting,hkSection)=self.accesshelper.getHotkey(name)
-
-					if mainHk=="" or mainHk.lower()=="none":
-						mainHk=""
-					name="btn_{}".format(name)
-					btn=self.widgets.get(name)
-					if isinstance(btn,QPushButton):
-						if btn.isVisible():
-							btn.show()
-							sigmap_run.setMapping(btn,name)
-							self.widgets.update({name:btn})
-							self.widgetsText.update({btn:{'mainHk':mainHk,'hkData':hkData,'hkSetting':hkSetting,'hkSection':hkSection}})
-							btn.clicked.connect(sigmap_run.map)
-							btn.setText(mainHk)
+						self.tblGrid.resizeRowToContents(row)
+		chk=QCheckBox("{} (Meta+=/Meta+-)".format(i18n.get("MAGNIFIEREFFECTS")))
+		chk.stateChanged.connect(self._setZoomOptionsEnabled)
+		row=self.tblGrid.rowCount()
+		self.tblGrid.setRowCount(row+1)
+		self.tblGrid.setCellWidget(row,0,chk)
+		self.tblGrid.resizeRowToContents(row)
+		for setting in zoomOptions:
+			row=self.tblGrid.rowCount()
+			self.tblGrid.setRowCount(row+1)
+			(name,data)=setting
+			desc=i18n.get(name.upper(),name)
+			btn=QRadioButton(desc)
+			self.tblGrid.setCellWidget(row,1,btn)
 		self._updateButtons()
 		self._setZoomOptionsEnabled()
 		return
 	#def _update_screen
 
 	def _setZoomOptionsEnabled(self):
+		return
 		if isinstance(self.widgets.get("magnifier",None),QCheckBox):
 			state=self.widgets.get("magnifier").isChecked()
 			for i in ["magnifierEnabled","zoomEnabled","lookingglassEnabled"]:
@@ -329,12 +215,14 @@ class access(confStack):
 	#def _updateConfig
 
 	def writeConfig(self):
+		return
 		plasmaConfig=self.plasmaConfig.copy()
 		for kfile in self.wrkFiles:
 			for section,data in plasmaConfig.get(kfile,{}).items():
 				dataTmp=[]
 				for setting,value in data:
-					btn=self.widgets.get(setting,'')
+					#btn=self.widgets.get(setting,'')
+					btn=""
 					if btn:
 						if isinstance(btn,QCheckBox) or isinstance(btn,QRadioButton):
 							#Optionbuttons depends on magnifier state
@@ -361,9 +249,10 @@ class access(confStack):
 		return
 
 	def _writeHotkeys(self):
+		return
 		self.plasmaConfig["kglobalshortcutsrc"]={}
 		for desc,widget in self.widgets.items():
-			if isinstance(widget,QPushButton):
+			if isinstance(widget,libhotkeys.QHotkeyButton):
 					desc=desc.replace("btn_","")
 					if desc.upper() in actionHk.keys():
 						(mainHk,hkData,hkSetting,hkSection)=self.accesshelper.getHotkey(actionHk[desc.upper()])

f56ffbf35986ecf548bc208e8f42a73d976ec5ce
juanma1980 <juanma1980@gmail.com>
2024-04-30 18:52:39 +0200

    enable and reconfigure, as kcm does

--- src/dock/loadScript.sh
+++ src/dock/loadScript.sh
@@ -5,28 +5,46 @@ TYPE=$(grep X-Plasma-API $METADATA)
 TYPE=${TYPE/*: /}
 TYPE=${TYPE//\"/}
 TYPE=${TYPE/,/}
-ID=$(grep X-Plasma-MainScript $METADATA)
+MAIN=$(grep X-Plasma-MainScript $METADATA)
+MAIN=${MAIN/*: /}
+MAIN=${MAIN//\"/}
+MAIN=${MAIN/,/}
+MAIN=${METADATA/metadata.json/contents\/${MAIN}}
+ID=$(grep \"Id\" $METADATA)
 ID=${ID/*: /}
 ID=${ID//\"/}
 ID=${ID/,/}
-ID=${METADATA/metadata.json/contents\/${ID}}
 LOADED=0
-UNLOAD=$(qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript $ID)
+UNLOAD="true"
 while [[ $UNLOAD == "true" ]]
 do
 	sleep 0.1
-	UNLOAD=$(qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript $ID)
-	LOADED=1
+	echo "Disabling $MAIN"
+	UNLOAD=$(qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript $MAIN)
+	echo $UNLOAD
+	if [[ $UNLOAD == "false" ]]
+	then
+		echo "Disabling $ID"
+		UNLOAD=$(qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript $ID)
+	fi
+	echo $UNLOAD
+	[[ $UNLOAD == "true" ]] && LOADED=1
 done
+echo $LOADED
 [[ ${LOADED} -ne 0 ]] && exit
+
+OUT=$(kwriteconfig5 --file kwinrc --group Plugins --key ${ID}Enabled true)
+echo $OUT
+qdbus org.kde.KWin /KWin org.kde.KWin.reconfigure
+exit 0
 
 if [[ $TYPE == "declarativescript" ]]
 then
-	CONT=$(qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadDeclarativeScript $ID)
-	echo "qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadDeclarativeScript $ID"
+	CONT=$(qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadDeclarativeScript $MAIN)
+	echo "qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadDeclarativeScript $MAIN"
 else
-	CONT=$(qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript $ID)
-	echo "qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript $ID"
+	CONT=$(qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript $MAIN)
+	echo "qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript $MAIN"
 fi
 echo "Launch $CONT"
 if [[ $? -ne 0 ]]

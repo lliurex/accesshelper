ccd189ea70ccb6cb18340757f7a55f8877ed3ddc
juanma1980 <juanma1980@gmail.com>
2024-04-30 08:17:29 +0200

    update translations

--- lliurex-po/generate_po.sh
+++ lliurex-po/generate_po.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PYTHON_FILES="../src/*.py ../src/stacks/*.py"
+PYTHON_FILES="../src/*.py ../src/stacks/*.py ../src/tools/*.py ../src/extras/*.py"
 
-mkdir -p access_helper
+mkdir -p accesswizard
 
-xgettext $UI_FILES $PYTHON_FILES -o access_helper/access_helper.pot
+xgettext $UI_FILES $PYTHON_FILES -o accesswizard/accesswizard.pot
 
 

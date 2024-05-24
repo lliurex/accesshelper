69e95895779ecd470c87e7a9459f3c2b8da7f6e7
juanma1980 <juanma1980@gmail.com>
2024-05-24 12:47:00 +0200

    WIP on i18n

--- lliurex-po/generate_po.sh
+++ lliurex-po/generate_po.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PYTHON_FILES="../src/*.py ../src/stacks/*.py ../src/tools/*.py ../src/dock/*.py ../src/dock/extras/*.py"
-
 mkdir -p accesswizard
-
 xgettext $UI_FILES $PYTHON_FILES -o accesswizard/accesswizard.pot
 
-
+QML_FILES="../kwin/plasmoids/net.lliurex.accesswizard/contents/ui/main.qml"
+mkdir -p plasma-accesswizard
+xgettext --from-code=UTF-8 -C --kde -ci18n $QML_FILES -o plasma-accesswizard/plasma_applet_net.lliurex.accesswizard.pot

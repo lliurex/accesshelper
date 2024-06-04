8c76ef5d8a89a86c6d6e4e172799689385fbbf93
juanma1980 <juanma1980@gmail.com>
2024-06-04 18:02:08 +0200

    expand if

--- src/tools/pulse-sddm.sh
+++ src/tools/pulse-sddm.sh
@@ -6,9 +6,13 @@ function debug()
 	[ $DBG -eq 1 ] && echo "$1" >> $LOG
 }
 debug "-----" 
-date 
-whoami
-[ -z "$(ps  -ef | grep $USER | grep a11y1)" ] && /usr/bin/dbus-launch /usr/libexec/at-spi2-registryd & >> $LOG && /usr/bin/dbus-launch /usr/libexec/at-spi-bus-launcher --a11y=1 --screen-reader=1 & >> $LOG
+debug "$(date)"
+debug "$(whoami)"
+if [ -z "$(ps  -ef | grep $USER | grep a11y1)" ] 
+then
+	/usr/bin/dbus-launch /usr/libexec/at-spi2-registryd & >> $LOG 
+	/usr/bin/dbus-launch /usr/libexec/at-spi-bus-launcher --a11y=1 --screen-reader=1 & >> $LOG
+fi
 pulseaudio -k || true
 dbus-launch pulseaudio --daemonize=no
 exit 0

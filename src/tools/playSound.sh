4d0690d392d0ba75fe8f9b9381dc9637f39702b7
juanma1980 <juanma1980@gmail.com>
2024-06-04 13:54:13 +0200

    SDDM ORCA WIP

--- src/tools/playSound.sh
+++ src/tools/playSound.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
+DBG=0
 LOG=/tmp/playSound-$(date +%Y%m%d_%H%M).log
-echo "-----" > $LOG
-echo "DSP: $0 $@">$LOG
+function debug()
+{
+	[ $DBG -eq 1 ] && echo "$1" >> $LOG
+}
+debug "-----"
+#debug"DSP: $0 $@">$LOG
 export DISPLAY=$1
 export XAUTHORITY=$2
-date >> $LOG
-whoami >> $LOG
-echo "Sleeping 2"
+debug "$(date)"
+debug "$(whoami)"
+#debug"Sleeping 2"
+#Sleep for avoiding race conditions with pulse 
 sleep 2
-#chmod 777 $LOG
-echo "XAUTH: $XAUTHORITY" >> $LOG
-#/usr/bin/orca --replace  >> $LOG &
-#su -m -c "/usr/bin/orca --replace" sddm >> $LOG
-#sudo -n -E -b -u sddm /usr/bin/orca --replace >> $LOG
+debug "XAUTH: $XAUTHORITY"
 pkexec --user sddm /usr/bin/mplayer /usr/share/sounds/Oxygen-Im-Phone-Ring.ogg >>$LOG 2>>$LOG
-#/usr/bin/mplayer /usr/share/sounds/Oxygen-Im-Phone-Ring.ogg >>$LOG 2>>$LOG
 sudo -n -u sddm /usr/bin/orca --replace >> $LOG
-echo "Orca: $?" >> $LOG
-echo "----" >> $LOG
-touch /tmp/.orca
+debug "Orca: $?"
+debug "----"
 exit 0

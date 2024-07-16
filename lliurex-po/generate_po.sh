7acc491386e56b3551a54ad53af0aa547a769187
lliurex <juanma1980@gmail.com>
2024-07-16 12:16:18 +0200

    update i18

--- lliurex-po/generate_po.sh
+++ lliurex-po/generate_po.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PYTHON_FILES="../src/*.py ../src/stacks/*.py ../src/tools/*.py ../src/dock/*.py ../src/dock/extras/*.py"
+PYTHON_FILES="../src/*.py ../src/stacks/*.py ../src/tools/*.py ../src/dock/*.py ../src/dock/extras/*.py ../kwin/scripts/kwin-ocrwindow/contents/ui/tts.py"
 mkdir -p accesswizard
 xgettext $UI_FILES $PYTHON_FILES -o accesswizard/accesswizard.pot
 

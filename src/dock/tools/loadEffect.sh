99518092cf8e70fef204f4f079c43b84eae07419
lliurex <juanma1980@gmail.com>
2026-04-07 09:50:39 +0200

    ID not mandatory for kwin6 effects

--- src/dock/tools/loadEffect.sh
+++ src/dock/tools/loadEffect.sh
@@ -5,7 +5,6 @@
 
 function toggle
 {
-	echo "$ID"
 	qdbus org.kde.KWin /Effects org.kde.kwin.Effects.toggleEffect $ID
 }
 
@@ -26,29 +25,23 @@ function unload
 
 function launchEffect
 {
-	echo "qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut $ENAME"
 	qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut "${ENAME}"
 }
 
 function enable
 {
-	echo "HEI"
 	if [[ $(qdbus org.kde.KWin /Effects org.kde.kwin.Effects.isEffectLoaded $ID) == "false" ]]
 	then
-		echo "HE1212I"
 		load
 		[[ $(qdbus org.kde.KWin /Effects org.kde.kwin.Effects.loadedEffects | grep $ID) ]] || toggle
 		[[ $(qdbus org.kde.KWin /Effects org.kde.kwin.Effects.activeEffects | grep $ID) ]] || launchEffect
 	else
-		echo "HE12I"
 		unload
 	fi
-	echo "HE2I"
 }
 
 function readMetadata
 {
-	echo "HOLA $ID $ENAME asasasasas"
 	ID=$(grep \"Id\" $METADATA)
 	ID=${ID/*: /}
 	ID=${ID//\"/}
@@ -57,8 +50,10 @@ function readMetadata
 	ENAME=${ENAME/*: /}
 	ENAME=${ENAME//\"/}
 	ENAME=${ENAME/,/}
+	[ ${#ID} -eq 0 ] && ID=${ENAME,,}
+
 	CHK_MAGNIFIERS=$(grep \"exclusiveGroup\" $METADATA)
-	[ ${#CHK_MAGNIFIERS} -ne 0 ] && NAME="view_zoom_in"
+	[ ${#CHK_MAGNIFIERS} -ne 0 ] && ENAME="view_zoom_in"
 }
 
 function readDir
@@ -68,7 +63,6 @@ function readDir
 }
 
 METADATA=$1
-echo "HOLA $ID $ENAME"
 [ -d $1 ] && readDir || readMetadata 
 [[ $(qdbus org.kde.KWin /Effects org.kde.kwin.Effects.activeEffects | grep $ID) ]] && toggle || enable
 exit 0
